031b31e1980376868688060677ea80f3456fcb6b
P. Taylor Goetz <tgoetz@monetate.com>
2024-06-19 17:26:48 -0400

    update file_owners migration.

--- backend/migrations/postgres/000005_create_file_owners_table.up.sql
+++ backend/migrations/postgres/000005_create_file_owners_table.up.sql
@@ -2,6 +2,10 @@ CREATE TABLE file_owners (
     file_id TEXT NOT NULL,
     assistant_id UUID,
     thread_id UUID,
+    file_path TEXT,
+    file_hash TEXT,
+    embedded BOOLEAN,
+    metadata JSONB,
     PRIMARY KEY (file_id),
     FOREIGN KEY (assistant_id) REFERENCES assistant(assistant_id) ON DELETE SET NULL,
     FOREIGN KEY (thread_id) REFERENCES thread(thread_id) ON DELETE SET NULL
